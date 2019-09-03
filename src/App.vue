6913d61dd503ee7c4e8b818c36d0501760374985
SeasSystem <jxae093663@163.com>
2019-09-03 08:59:16 +0800

    '初始化结构'

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="#app">
+    <div class="todo-container">
+      <todo-header></todo-header>
+    </div>
+  </div>
+</template>
+
+<script>
+  import Header from './components/Header';
+
+  export default {
+    name: "App",
+    components: {
+      'todo-header': Header
+    }
+  }
+</script>
+
+<style scoped>
+  .todo-container {
+    width: 600px;
+    margin: 0 auto;
+  }
+  .todo-container .todo-wrap {
+    padding: 10px;
+    border: 1px solid #ddd;
+    border-radius: 5px;
+  }
+</style>
